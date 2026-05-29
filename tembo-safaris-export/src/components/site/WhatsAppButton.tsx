84d327feaa6c67dedc2c7cda83124c324da07b51
Lomelock <lomelocklaizer@gmail.com>
2026-05-29 10:56:06 +0000

    UPDATED

--- tembo-safaris-export/src/components/site/WhatsAppButton.tsx
+++ tembo-safaris-export/src/components/site/WhatsAppButton.tsx
@@ -1,4 +1,23 @@
-import { MessageCircle } from "lucide-react";
+/// <reference types="react" />
+import * as React from "react";
+
+function MessageCircle(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      aria-hidden="true"
+      {...props}
+    >
+      <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-4.7a8.38 8.38 0 0 1-.9-3.8A8.5 8.5 0 0 1 12 3a8.5 8.5 0 0 1 9 8.5z" />
+    </svg>
+  );
+}
 
 export function WhatsAppButton() {
   return (
